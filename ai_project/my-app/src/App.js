61fccfeb2685de6f5c3e567001400831eef5215b
Yaroslav <yaroslav.brila@gmail.com>
2020-11-18 18:26:20 +0200

    login_form_update

--- ai_project/my-app/src/App.js
+++ ai_project/my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import RegistrationPage from "./components/RegistrationPage";
+import LoginPage from "./components/login_page/LoginPage"
 const App = () => {
   return (
     <div className="App">
-        <RegistrationPage />
+        <LoginPage />
+        {/*<RegistrationPage/>*/}
     </div>
   )
 }
--- ai_project/my-app/src/components/RegistrationPage.jsx
+++ ai_project/my-app/src/components/RegistrationPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Formik, Field, ErrorMessage, Form,} from "formik";
 import *as Yup from "yup"
+//import classes from "./Registration_index.module.css"
 
 
 const validationSchema = Yup.object().shape({
@@ -14,83 +15,72 @@ const validationSchema = Yup.object().shape({
     password: Yup.string()
         .required('Required')
 });
-
-
-
 const RegistrationPage = () => {
-    return(
-    <div>
-        <h1>Fill up the registration form</h1>
-        <Formik
-            initialValues={{firstName: '', lastName: '', email: '', password: ''}}
-            validationSchema={validationSchema}
-            onSubmit={(values) => {
-                console.log(values)
-            }}
-        >
-            {({errors, touched}) => (
-                <Form>
-                    <div className= "registrationForm">
-                        <div>
-                            <div><label>First name </label></div>
-                            <Field
-                                placeholder = "first name"
-                                name = "firstName"
-                                type = "input"
-                            />
+    return (
+        <div>
+            <header >
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg" alt=""/>
+                <p></p>
+            <h1 >Fill up the registration form</h1>
+            </header>
+            <Formik
+                initialValues={{firstName: '', lastName: '', email: '', password: ''}}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}>
+                {({errors, touched}) => (
+                    <Form >
+                        <div >
+                            <div>
+                                <div><label>First name </label></div>
+                                <Field
+                                    placeholder="first name"
+                                    name="firstName"
+                                    type="input"
+                                />
 
-                            <ErrorMessage name = "firstName"/>
+                                <ErrorMessage name="firstName"/>
+                            </div>
+                            <div>
+                                <div><label>Last name </label></div>
+                                <Field
+                                    placeholder="last name"
+                                    name="lastName"
+                                    type="input"
+                                />
+                                <ErrorMessage name="lastName"/>
+                            </div>
+                            <div>
+                                <div    ><label>E-mail </label></div>
+                                <Field
+                                    placeholder="email@smth.com"
+                                    name="email"
+                                    type="email"
+                                />
+                                <ErrorMessage name="email"/>
+                            </div>
+                            <div>
+                                <div><label>Password </label></div>
+                                <Field
+                                    placeholder="password"
+                                    name="password"
+                                    type="password"
+                                />
+                                <ErrorMessage name="password"/>
+                            </div>
                         </div>
                         <div>
-                            <div><label>Last name </label></div>
-                            <Field
-                                placeholder = "last name"
-                                name = "lastName"
-                                type = "input"
-                            />
-                            <ErrorMessage name = "lastName"/>
+                            <button type="submit">
+                                Sign Up!
+                            </button>
                         </div>
-                        <div>
-                            <div><label>E-mail </label></div>
-                            <Field
-                                placeholder = "email@smth.com"
-                                name = "email"
-                                type = "email"
-                            />
-                            <ErrorMessage name = "email"/>
-                        </div>
-                        <div>
-                            <div><label>Password </label></div>
-                            <Field
-                                placeholder = "password"
-                                name = "password"
-                                type = "password"
-                            />
-                            <ErrorMessage name = "password"/>
-                        </div>
-
-
-                    </div>
-
-                    <div>
-                        <button type ="submit">
-                            Sign Up!
-                        </button>
-                    </div>
-                </Form>
+                    </Form>
                 )}
-
-
-        </Formik>
-
-
-
-    </div>
-
+            </Formik>
+        </div>
     )
-
 }
-
 export default RegistrationPage
 
 
